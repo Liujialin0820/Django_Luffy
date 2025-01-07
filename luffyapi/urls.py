7bf604b4c97e0bfeea5ab7f1b1b358468d536793
jialin <mr.liujalo@gmail.com>
2025-01-07 20:25:09 +0800

    1.5 项目创建和目录调整

--- luffyapi/urls.py
+++ luffyapi/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for settings project.
+URL configuration for luffyapi project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
